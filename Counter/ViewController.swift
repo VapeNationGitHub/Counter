ed9a9e8b69801ec3206a6c7be6c20ec6bd2cec35
Helper <helper@email.com>
2025-01-09 18:56:01 +0800

    Выполнена базовая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Переменные counterLabel и buttonCounter связываются с объектами из интерфейса UILabel и UIButton соотвественно
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var buttonCounter: UIButton!
+    // Переменная для хранения значения счетчика
+    private var counterValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Устанавливаем и выводим начальное значение 0 для переменной counterValue; меняем цвет кнопки на серый, а цвет счетчки на красный
+        counterValue = 0
+        counterLabel.text = String("Значение счетчика: \(counterValue)")
+        buttonCounter.tintColor = .gray
+        counterLabel.textColor = .red
     }
-
-
+    
+    // Связываем кнопку с действием
+    @IBAction func incrementButton(_ sender: Any) {
+        
+        // При нажатии на кнопку, переменная counterValue увеличивается на 1
+        counterValue += 1
+        counterLabel.text = String("Значение счетчика: \(counterValue)")
+        
+        // Выводим логи в консоль (для самопроверки)
+        print("Значение счетчика (логи): \(counterValue)")
+        
+    }
 }
-
